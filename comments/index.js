4f0e75796428ad65f443eda1e24be99fa07cc41f
Nabcellent <nabcellent.dev@gmail.com>
2021-12-01 11:44:48 +0300

    App: Created Moderation service
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const {randomBytes} = require('crypto')
+const { randomBytes } = require('crypto')
 const axios = require("axios");
-const {param} = require("express/lib/router");
+const { param } = require("express/lib/router");
 
 const app = express();
 
@@ -15,35 +15,35 @@ app.use(express.urlencoded({ extended: false }));
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || [])
+	res.send(commentsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', async (req, res) => {
-    const {content} = req.body
-    const commentId = randomBytes(4).toString('hex')
+app.post('/posts/:id/comments', async(req, res) => {
+	const { content } = req.body
+	const commentId = randomBytes(4).toString('hex')
 
-    const comments = commentsByPostId[req.params.id] || []
+	const comments = commentsByPostId[req.params.id] || []
 
-    comments.push({id: commentId, content})
+	comments.push({ id: commentId, content, status: 'pending' })
 
-    commentsByPostId[req.params.id] = comments
+	commentsByPostId[req.params.id] = comments
 
-    await axios.post(`http://localhost:4005/events`, {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            postId: req.params.id,
-            content
-        }
-    })
+	await axios.post(`http://localhost:4005/events`, {
+		type: 'CommentCreated',
+		data: {
+			id: commentId,
+			postId: req.params.id,
+			content
+		}
+	})
 
-    res.status(201).send(comments)
+	res.status(201).send(comments)
 })
 
 app.post('/events', (req, res) => {
-    console.log(`Received event: `, req.body.type)
+	console.log(`Received event: `, req.body.type)
 
-    res.status(200).send({})
+	res.status(200).send({})
 })
 
 app.listen(4001, () => console.log('Listening on port 4001'))
