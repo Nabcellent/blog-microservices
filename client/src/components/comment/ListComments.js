e45f98481ac350d1b0ceff9e6ae7d5df205bd871
Nabcellent <nabcellent.dev@gmail.com>
2021-12-01 12:40:25 +0300

    App: Modified React App
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- client/src/components/comment/ListComments.js
+++ client/src/components/comment/ListComments.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-const ListComments = ({comments}) => {
-    const renderedComments = Object.values(comments).map(comment => {
-        return <li key={comment.id}>
-            --- {comment.content}
-        </li>
-    })
+const ListComments = ({ comments }) => {
+	const renderedComments = Object.values(comments).map(comment => {
+		let { content } = comment;
 
-    return <ul>{renderedComments}</ul>
+		if(comment.status === 'approved') {
+			content = comment.content
+		} else if(comment.status === 'pending') {
+			content = <span className={'text-secondary'}>Awaiting moderation...</span>
+		} else if(comment.status === 'rejected') {
+			content = <i className={'text-danger'}>Comment rejected!</i>
+		}
+
+		return <li key={comment.id}>--- {content}</li>
+	})
+
+	return <ul>{renderedComments}</ul>
 }
 
 export default ListComments
