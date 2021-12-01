884ec78bd04b49e6db91c2674f8248e9ed84acc7
Nabcellent <nabcellent.dev@gmail.com>
2021-12-01 11:50:28 +0300

    App: Implementing Moderation service
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- moderation/index.js
+++ moderation/index.js
@@ -7,7 +7,19 @@ const app = express()
 app.use(bodyParser.json())
 
 app.post('/events', (req, res) => {
+	const { type, data } = req.body
 
+	if(type === 'CommentCreated') {
+		const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+		axios.post(`http://localhost:4005`, {
+			type: 'CommentModerated',
+			data: {
+				...data,
+				status
+			}
+		})
+	}
 })
 
 app.listen(4003, () => console.log(`App listening on port: 4003`))
