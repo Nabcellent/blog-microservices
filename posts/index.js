58c87c4e9069f6bd586f19c5f5ecf17520eca9bd
Nabcellent <nabcellent.dev@gmail.com>
2021-12-01 11:47:25 +0300

    App: Implementing Moderation service
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const {randomBytes} = require('crypto')
+const { randomBytes } = require('crypto')
 const axios = require("axios");
 
 const app = express();
@@ -14,31 +14,31 @@ app.use(cors())
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts)
+	res.send(posts)
 })
 
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex')
-    const {title} = req.body
+app.post('/posts', async(req, res) => {
+	const id = randomBytes(4).toString('hex')
+	const { title } = req.body
 
-    posts[id] = {
-        id, title
-    }
+	posts[id] = {
+		id, title
+	}
 
-    await axios.post(`http://localhost:4005/events`, {
-        type: 'PostCreated',
-        data: {
-            id, title
-        }
-    })
+	await axios.post(`http://localhost:4005/events`, {
+		type: 'PostCreated',
+		data: {
+			id, title
+		}
+	})
 
-    res.status(201).send(posts[id])
+	res.status(201).send(posts[id])
 })
 
 app.post('/events', (req, res) => {
-    console.log('Received event: ', req.body.type)
+	console.log('Received event: ', req.body.type)
 
-    res.status(200).send({})
+	res.status(200).send({})
 })
 
 app.listen(4000, () => console.log('Listening on port 4000'))
