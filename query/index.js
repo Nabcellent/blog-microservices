5cf48ebdbec2625c1346d8e3691b362810cd14bf
Nabcellent <nabcellent.dev@gmail.com>
2021-12-01 13:03:05 +0300

    App: Implemented event storage and sync
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- query/index.js
+++ query/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const { response } = require("express");
+const axios = require("axios");
 
 const app = express()
 
@@ -9,13 +11,7 @@ app.use(cors())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
-	res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-	const { type, data } = req.body
-
+const handleEvent  = (type, data) => {
 	if(type === 'PostCreated') {
 		const { id, title } = data
 
@@ -39,10 +35,28 @@ app.post('/events', (req, res) => {
 		comment.status = status
 		comment.content = content
 	}
+}
 
-	console.log(posts)
+app.get('/posts', (req, res) => {
+	res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+	const { type, data } = req.body
+
+	handleEvent(type, data)
 
 	res.status(200).send({})
 })
 
-app.listen(4002, () => console.log(`Listening on port 4002`))
+app.listen(4002, () => {
+	console.log(`Listening on port 4002`)
+
+	axios.get(`http://localhost:4005/events`).then(response => {
+		for(let event of response.data) {
+			console.log(`Processing event...: ${event.type}`)
+
+			handleEvent(event.type, event.data)
+		}
+	})
+})
